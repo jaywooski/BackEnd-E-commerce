6b7a3a7dd0903db2deaa1bc45548a0ac0da9024c
Woogster <john.m.wooley@gmail.com>
2022-03-23 02:53:40 -0500

    Adding routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,20 +5,66 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    // be sure to include its associated Products
+    include: [{
+      model: Product,
+      where: {
+        id: category_id
+      }
+    }] 
+  })
+  .then(foundCategories => res.json(foundCategories))
+  .catch((err) => {
+    console.log(err);
+    res.json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }, 
+    // be sure to include its associated Products
+    include: [{
+      model: Product,
+      where: {
+        id: category_id
+      }
+    }]
+  })
+  .then(foundCategories => res.json(foundCategories))
+  .catch(err => console.log(err));
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(madeCategories => res.json(madeCategories))
+  .catch(err => console.log(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(categoryInfo => res.json(categoryInfo))
+  .catch((err) => {
+    console.log(err);
+    res.json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
